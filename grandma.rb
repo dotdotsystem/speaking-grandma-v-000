fa1137c6a7dabe468062e27feee803a292182d40
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-17 22:14:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(speak)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif speak == speak.downcase()
     return "HUH?! SPEAK UP, SONNY!"
-  elsif speak == speak.upcasse()
+  elsif speak == speak.upcase()
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
